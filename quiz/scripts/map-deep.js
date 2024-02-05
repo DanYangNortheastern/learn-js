9ce0764b62c8e2fbe7191bc7a0a1a8d303da0721
yang.dani <yang.dani@northeastern.edu>
2024-02-05 16:16:01 -0500

    completed items

--- quiz/scripts/map-deep.js
+++ quiz/scripts/map-deep.js
@@ -5,14 +5,20 @@ const originalMatrix = [
     [7, 9, 8]
   ];
   
-  const newMatrix = originalMatrix.map((row) => {
-    return row.sort();
+  const newMatrix1 = originalMatrix.map((row) => {
+    let rowCopy = Array.from(row)
+    return rowCopy.sort();
   });
-  
+
+  const newMatrix2 = originalMatrix.map((row) => {
+    return [...row].sort();
+  });
+
   // Modifying the original matrix (changing the last element of the first row)
   originalMatrix[0][2] = 99;
   
   // Displaying the original and new matrices
   console.log('Original Matrix (modified):', originalMatrix);
-  console.log('New Matrix (unaffected):', newMatrix);
+  console.log('New Matrix1 (unaffected):', newMatrix1);
+  console.log('New Matrix2 (unaffected):', newMatrix2);
   
